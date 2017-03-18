ab5e4a6ddd04c34e6922b105bc8ef6c51457c4c6
paudyals <paudyals@192.168.0.77>
2017-03-17 21:24:03 -0500

    Added Unreferenced Sentence Service

---
+++ src/com/icodejava/research/nlp/services/UnreferencedSentenceService.java
@@ -0,0 +1,49 @@
+package com.icodejava.research.nlp.services;
+
+import java.util.List;
+
+import com.icodejava.research.nlp.NPTokenizer;
+import com.icodejava.research.nlp.database.ArticlesDB;
+import com.icodejava.research.nlp.database.Tables;
+import com.icodejava.research.nlp.database.WordsUnreferencedDB;
+
+public class UnreferencedSentenceService {
+	
+	public static void main (String args []) {
+		processUnreferencedSentences(10000);
+	}
+
+	private static void processUnreferencedSentences(int articleLimit) {
+		List<Integer> unprocessedArticlesID =  ArticlesDB.selectArticlesUnProcessed(articleLimit);
+
+		int processing = 0;
+		for(Integer id: unprocessedArticlesID) {
+			
+			System.out.println("Processing record " + processing++ + " out of " + unprocessedArticlesID.size());
+			String articleText = ArticlesDB.selectArticleTextByID(id);
+			
+			//tokenize 
+			List<String> words = NPTokenizer.tokenizeWords(articleText);
+			
+			//then store the words to Database
+			for(String word:words) {
+				WordsUnreferencedDB.insertWord(word);
+			}
+			
+			
+			WordsUnreferencedDB.getRowCount(WordsUnreferencedDB.DATABASE_URL, Tables.WORDS_UNREFERENCED);
+			
+			//then mark article as processed.
+			ArticlesDB.updateArticleMarkAsProcessedForUnreferenceWord(id);
+			
+		}
+		
+		//do a second round of cleaning
+		WordsUnreferencedDB.cleanStrangeWords();
+		
+		//finally print report
+		ReportingService.printUnreferencedWordReport();
+		
+	}
+
+}
