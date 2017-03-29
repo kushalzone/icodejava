88df8ccba215c18b8281737831d66e9c43b1ccae
paudyals <paudyals@192.168.0.77>
2017-03-28 22:52:35 -0500

    Nepali Grammar File

---
+++ src/com/icodejava/research/nlp/database/Grammar.java
@@ -0,0 +1,33 @@
+package com.icodejava.research.nlp.database;
+
+public class Grammar {
+
+
+	/**
+	 * Terms that give attributes to nouns, extending their definitions.
+	 * 
+	 * E.g. अग्लो
+	 */
+	public static final String ADJECTIVE = "ADJECTIVE"; //Bisheshan
+	public static final String ADVERB = "ADVERB"; //Kriya Bisheshan. E.g. Yo, Tyo, Tini, Uni
+	public static final String CONJUNCTION = "CONJUNCTION";
+	public static final String DETERMINER = "DETERMINER";
+	public static final String INJTERJECTION = "INTERJECTION";
+	public static final String MORPHEME = "MORPHEME";
+	public static final String NOUN = "NOUN";//Naam or Sangya - Jaati Bachak, Byakti Bachak, samuha bachak, draba bachak, bhab bachak
+	public static final String NUMERAL = "NUMERAL";
+	public static final String PARTICLE = "PARTICLE";
+	public static final String PHRASE = "PHRASE"; //Bakyanksha
+	public static final String PREPOSITION = "PREPOSITION";
+	public static final String POSTPOSITION = "POSTPOSITION";
+	public static final String PRONOUN = "PRONOUN"; //Sarba Nam: Ma, Hami, Timi
+	public static final String VERB = "VERB"; //Kriya
+	
+	
+	//Past Tense
+	//Present Tense
+	//Future Tense
+	//Anukaranatmak
+	//Idiom - Bakpaddhati
+
+}
